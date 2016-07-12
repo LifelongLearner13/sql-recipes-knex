68ed4fc4c31d0d9b426fd837cee262dadcc3a5ed
Sierra <sierragregg@Gmail.com>
2016-07-12 02:00:15 +0000

    Finished push endpoint and started get endpoint

--- index.js
+++ index.js
@@ -16,15 +16,13 @@ var app = express();
 
 /*----------- USER ENDPOINTS ----------*/
 app.post('/recipes', jsonParser, function(request, response) {
-    var tagsInsertArray = request.body.tags.map(function(currentValue) {
-        return {
-            tag: currentValue
-        };
-    });
 
+    // First, insert the recipe name into the RECIPES table. The id of the newly added recipe
+    // is required to complete the insert into the STEPS and RECIPES_TAGS tables.
     knex.insert({
         name: request.body.name
     }).into('recipes').returning('id').then(function(recipeId) {
+        
         // Use the array from the JSON file to create a new array of objects that can be inserted into the database
         var stepsInsertArray = request.body.steps.map(function(currentValue) {
             return {
@@ -36,33 +34,42 @@ app.post('/recipes', jsonParser, function(request, response) {
         // Use KNEX to insert all steps into the STEPS tabel
         knex.insert(stepsInsertArray).into('steps').then();
 
-        // Select given tags in database.
-        // SELECT id FROM tags WHERE tag in tag_array_from_json;
-        // If a tag already exists, then associate it with the current recipe in the RECIPES_TAGS table
-        // Else insert tag into TAG table, then associate it with the current recipe in the RECIPES_TAGS table
+        // Loop through all tags given in JSON.
+        for (var tag in request.body.tags) {
+            // Generate a temporary table containing the tag name only if it doesn't already exist in the TAGS table.
+            // Insert the value from the temporary table into the TAGS table. This means only tags that don't already 
+            // exist in the table, will be added. Finally, if an insert was made, return the id of the new row.
+            knex.raw('insert into tags (tag) select * from (values (?)) as newTags except (select tag from tags) returning id;', request.body.tags[tag]).then(function(response) {
+                // knex.raw returns an object containing whatever the SQL statment would have return if executed on the database.
+                // If a tag was inserted, the new id will be stored in response.rows[0].id.
+                // Check if an insert occured (i.e. response.rows[0] is not an empty array).
+                if(response.rows[0]) {
+                    // If a new tag was added, associate it with the current recipe in the RECIPES_TAGS table
+                    knex.insert({tag_id: response.rows[0].id, recipe_id: recipeId[0]}).into('recipes_tags').then();
+                } else {
+                    console.log('dont insert');
+                }
+            });
+        }
         
-        //knex.raw('insert into tags (tag) select * from (values (?) as newTags) except (select tag from tags);', [request.body.tags]).then();
-        
-        // for newtag in request.body.tags: select * from tags where tag = $newtag$ --> if 0 results, insert into tags (tag) values ($newtag$)
-        
-        // Associates all tags that already exist in the TAGS table with the current recipe
-        // The nested select creates a temporary table which the values are inserted into
-        // insert into recipes_tags select $recipe_id$, id from tags where tag in ('tag1', 'tag2', 'tag3');
-        knex.insert(knex.select(recipeId[0], 'id').from('tags').whereIn('tag', request.body.tags)).into('recipes_tags').then();
-        
+        // Cannot guarantee that all inserts have finished, but need to send a response somewhere.
+        response.sendStatus(200);
     });
 });
 
-// {
-//     name: 'Khichidi Kadhi',
-//     steps: [
-//         'Cook rice and lentils',
-//         'Thicken yogurt with gram flour, cooking over a low heat',
-//         'Add tadka to yogurt'
-//     ],
-//     tags: ['curry', 'vegetarian']
-// }
+app.get('/recipes', function(request, response) {
+    var queryResponse = [];
+    
+    // select recipes.id, recipes.name, steps.description from recipes join steps on recipes.id = steps.recipe_id ;
+    // select recipes.id, recipes.name, tags.tag from recipes right join recipes_tags on recipes.id = recipes_tags.recipe_id left join tags on recipes_tags.tag_id = tags.id;
+    knex.select('recipes.id', 'recipes.name', 'steps.description').from('recipes').join('steps', {'recipes.id': 'steps.recipe_id'}).then(function(dbResponse) {
+       for(var i in dbResponse){
+           console.log(dbResponse[i]);
+       }
+    });
+});
 
+/* ----------- ADD LISTENER --------------- */
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
     console.log('Listening on port:' + port);
